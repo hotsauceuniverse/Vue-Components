57a123dca8a7b46d73440b09377a6ba8e05bfeb1
장 세영(Jang Seyoung) <92082361+hotsauceuniverse@users.noreply.github.com>
2021-12-12 23:59:04 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,38 @@
+<template>
+<MyBtn :color="color" text="Banana" />
+<MyBtn color="royalblue" text="Apple" />
+<MyBtn large text="Cherry" />
+<MyBtn text="Melon" /><br>
+<Btn @click="log" @change-msg="logMsg">Seyoung</Btn>
+
+<Parent :message="message" />
+</template>
+
+<script>
+import MyBtn from './components/MyBtn'
+import Btn from './components/Btn'
+
+export default {
+  components: {
+    MyBtn,
+    Btn
+  },
+
+  methods: {
+    log() {
+      console.log('click')
+    },
+
+    logMsg(msg) {
+      console.log(msg)
+    },
+
+  data() {
+    return{
+      color: '#000',
+      }
+    },
+
+  }
+}
+</script>
---
+++ MyBtn.vue
@@ -0,0 +1,44 @@
+<template>
+<div :class="{ large }" :style="{ backgroundColor: color }" class="btn">{{ text }}</div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'gray'
+    },
+
+    large: {
+      type: Boolean,
+      default: false
+    },
+
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
+  .btn {
+    display: inline-block;
+    margin: 4px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    background-color: gray;
+    color: white;
+    cursor: pointer;
+  }
+
+  .btn.large {
+    font-size: 20px;
+    padding: 10px 20px;
+  }
+</style>
